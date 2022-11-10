3ca71b3e00eb5c6b52d71ec75d3c051ae56f036e
emirrozerr <emirrozerr@hotmail.com>
2022-11-10 23:56:30 +0300

    Week 3 added

---
+++ Week-3/flutter_responsive_ui/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_ui/LoginExample.dart';
+import 'package:flutter_responsive_ui/FlexibleExample.dart';
+import 'package:flutter_responsive_ui/FittedBoxExample.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: LoginExample(),
+));
